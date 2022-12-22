dbaf9d293056b24c275241fa8fe0b2970fb01173
Mantas <Mantasb77@gmail.com>
2022-12-22 14:56:24 +0000

    Added LightRedDisabled color

--- app/src/main/java/com/kotlisoft/chatui/presentation/ui/theme/Color.kt
+++ app/src/main/java/com/kotlisoft/chatui/presentation/ui/theme/Color.kt
@@ -4,6 +4,7 @@ import androidx.compose.ui.graphics.Color
 
 val LightWhite = Color(0xFFF2F5FB)
 val LightRed = Color(0xFFFF1278)
+val LightRedDisabled = Color(0xFFFF93BD)
 val TextColor = Color(0xFF607180)
 val TimestampColor = Color(0xFFADB5BC)
 val ThreeDotsColor = Color(0xFFC6CCD3)
