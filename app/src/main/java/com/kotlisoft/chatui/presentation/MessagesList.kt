01cb0b58e27783ad86a8dffda8b088803a758283
Mantas <Mantasb77@gmail.com>
2022-12-22 14:39:18 +0000

    Clear focus when on messages area clicked

--- app/src/main/java/com/kotlisoft/chatui/presentation/MessagesList.kt
+++ app/src/main/java/com/kotlisoft/chatui/presentation/MessagesList.kt
@@ -1,11 +1,13 @@
 package com.kotlisoft.chatui.presentation
 
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.LazyListState
 import androidx.compose.foundation.lazy.items
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalFocusManager
 import androidx.compose.ui.unit.Dp
 import com.kotlisoft.chatui.domain.models.ChatMessage
 
@@ -16,10 +18,14 @@ fun MessagesList(
     senderName: String,
     bottomPadding: Dp
 ) {
+    val focusManager = LocalFocusManager.current
     Box(
         modifier = Modifier
             .fillMaxSize()
             .padding(bottom = bottomPadding)
+            .clickable {
+                focusManager.clearFocus()
+            }
     ) {
         LazyColumn(state = listState) {
             items(items = messages) { message ->
